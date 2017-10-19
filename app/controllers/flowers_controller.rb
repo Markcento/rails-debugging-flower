0ec6431fee14e8e97fe4b2d23cd5e40195b3979e
Mark <Mark@MTC.local>
2017-10-19 09:28:51 -0700

    added conditions to catch special characters and numbers in the paramater fields

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -12,12 +12,13 @@ class FlowersController < ApplicationController
 
     @flower = Flower.create(flower_params)
     @families = Family.all
-    if @flower.valid? 
+    if @flower.valid? == false ||  @flower.name.include?("!") || @flower.name.include?("#") || @flower.name.include?("$") ||  @flower.name.include?("@") || @flower.name.include?("%") || @flower.name.include?("^") || @flower.name.include?("&") ||  @flower.name.include?("*") || @flower.name.include?("1") || @flower.name.include?("2") || @flower.name.include?("3") ||  @flower.name.include?("4") || @flower.name.include?("5") || @flower.name.include?("6") ||  @flower.name.include?("7") ||  @flower.name.include?("8") || @flower.name.include?("9") || @flower.name.include?("0") || @flower.color.include?("!") || @flower.color.include?("#") || @flower.color.include?("$") ||  @flower.color.include?("@") || @flower.color.include?("%") || @flower.color.include?("^") || @flower.color.include?("&") ||  @flower.color.include?("*") || @flower.color.include?("1") || @flower.color.include?("2") || @flower.color.include?("3") ||  @flower.color.include?("4") || @flower.color.include?("5") || @flower.color.include?("6") ||  @flower.color.include?("7") ||  @flower.color.include?("8") || @flower.color.include?("9") || @flower.color.include?("0")  
+      flash.now[:notice] = "Unable to create flower"
+      @flower.destroy
+      render :new
+    else
       redirect_to flowers_path
       flash[:notice] = "Flower was successfully created."
-    else
-      flash.now[:notice] = "Unable to create flower"
-      render :new
     end
   end
 
--- spec/features/new_flower_spec.rb
+++ spec/features/new_flower_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "Create a flower", :type => :feature do
   end
 
   let!(:family){ Family.create(name: 'Astor')}
-  scenario "User creates a new flower" do
+  scenario "User creates a new flower with blank name" do
     visit "/flowers/new"
 
     fill_in "Name", :with => ""
@@ -26,7 +26,7 @@ RSpec.feature "Create a flower", :type => :feature do
   end
 
   let!(:family){ Family.create(name: 'Astor')}
-  scenario "User creates a new flower" do
+  scenario "User creates a new flower with blank color" do
     visit "/flowers/new"
 
     fill_in "Name", :with => "Tulip"
@@ -35,6 +35,54 @@ RSpec.feature "Create a flower", :type => :feature do
     click_button "Save"
 
     expect(page).to have_text("There were some problems saving the form.")
+  end
+
+  let!(:family){ Family.create(name: 'Astor')}
+  scenario "User creates a new flower with a number in the name" do
+    visit "/flowers/new"
+
+    fill_in "Name", :with => "2lip"
+    fill_in "Color", :with => "white"
+    page.check 'edible'
+    click_button "Save"
+
+    expect(page).to have_text("Unable to create flower")
+  end
+
+  let!(:family){ Family.create(name: 'Astor')}
+  scenario "User creates a new flower with a number in the color" do
+    visit "/flowers/new"
+
+    fill_in "Name", :with => "Tulip"
+    fill_in "Color", :with => "gr33n"
+    page.check 'edible'
+    click_button "Save"
+
+    expect(page).to have_text("Unable to create flower")
+  end
+
+  let!(:family){ Family.create(name: 'Astor')}
+  scenario "User creates a new flower with a special character in the name" do
+    visit "/flowers/new"
+
+    fill_in "Name", :with => "d@fodil"
+    fill_in "Color", :with => "yellow"
+    page.check 'edible'
+    click_button "Save"
+
+    expect(page).to have_text("Unable to create flower")
+  end
+
+  let!(:family){ Family.create(name: 'Astor')}
+  scenario "User creates a new flower with a special character in the color" do
+    visit "/flowers/new"
+
+    fill_in "Name", :with => "Tulip"
+    fill_in "Color", :with => "or@nge"
+    page.check 'edible'
+    click_button "Save"
+
+    expect(page).to have_text("Unable to create flower")
   end
 
 end
